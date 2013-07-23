167acc48d75f64ed327e810de7c57944b95b5c5f
Lucas Theis <lucas@theis.io>
2013-07-23 16:27:12 +0200

    Added script.

---
+++ predict.sh
@@ -0,0 +1,21 @@
+# train STMs
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+./code/experiments/predict.py --num_components 2 --num_features 4 --num_valid 3
+
+# train GLMs
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
+./code/experiments/predict.py --num_components 1 --num_features 0 --num_valid 3
