Fixed calculation of correlations.

code/experiments/average.py
@@ -8,7 +8,7 @@ sys.path.append('./code')
 
 from argparse import ArgumentParser
 from glob import glob
-from numpy import corrcoef, hstack
+from numpy import corrcoef, vstack, hstack, std, mean, sqrt
 from scipy.io import loadmat, savemat
 from tools import Experiment
 from cmt.models import GLM, STM
@@ -35,14 +35,9 @@ def main(argv):
 		if i + 1 not in cells:
 			outputs_test.append(outputs[-1])
 
-	# average predictions
-	counter = 0.
-
-	predictions = 0
-	predictions_test = 0
-
-	correlations = []
-	correlations_test = []
+	# load predictions
+	predictions = [[] for _ in range(data.shape[0])]
+	predictions_test = [[] for _ in range(data.shape[0] - len(cells))]
 
 	model_type = GLM if args.model.upper() == 'GLM' else STM
 
@@ -50,23 +45,33 @@ def main(argv):
 		results = Experiment(filepath)
 
 		if isinstance(results['model'], model_type):
-			predictions      = predictions      + hstack(results['predictions'])
-			predictions_test = predictions_test + hstack(results['predictions_test'])
-			counter += 1.
+			for i in range(len(predictions)):
+				predictions[i].append(results['predictions'][i])
 
-	predictions /= counter
-	predictions_test /= counter
+			for i in range(len(predictions_test)):
+				predictions_test[i].append(results['predictions_test'][i])
 
-	corr      = corrcoef(hstack(outputs),      predictions)[0, 1]
-	corr_test = corrcoef(hstack(outputs_test), predictions_test)[0, 1]
+	corr = []
+	corr_test = []
 
-	print 'Average correlation:'
-	print '\t{0:.5f} (test)'.format(corr_test)
-	print '\t{0:.5f} (total)'.format(corr)
+	# average predictions and compute correlations
+	for i in range(len(predictions)):
+		predictions[i] = mean(vstack(predictions[i]), 0).reshape(1, -1)
+		corr.append(corrcoef(outputs[i], predictions[i])[0, 1])
+
+	for i in range(len(predictions_test)):
+		predictions_test[i] = mean(vstack(predictions_test[i]), 0).reshape(1, -1)
+		corr_test.append(corrcoef(outputs_test[i], predictions_test[i])[0, 1])
+
+	sem      = std(corr) / sqrt(len(corr))
+	sem_test = std(corr_test) / sqrt(len(corr_test))
 
 	print 'Correlation of average response:'
-	print '\t{0:.5f} (test)'.format(corr_test)
-	print '\t{0:.5f} (total)'.format(corr)
+	print '\t{0:.5f} (test)'.format(mean(corr_test))
+	print '\t{0:.5f} (total)'.format(mean(corr))
+
+	savemat('results/predictions.{0}.mat'.format(args.model.lower()), {'predictions': predictions},
+		oned_as='row', do_compression=True)
 
 	return 0
 