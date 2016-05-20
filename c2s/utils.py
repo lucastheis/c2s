f2c94b9adc0d740e2665bc04dfb1420fd72c85ba
Lucas Theis <lucas@theis.io>
2016-05-20 12:58:56 +0100

    Added missing utils.py

---
+++ c2s/utils.py
@@ -0,0 +1,13 @@
+def convert(input):
+	"""
+	Convert dictionary keys/values into something that can be saved with `scipy.io.savemat`.
+	"""
+
+	if isinstance(input, dict):
+		return dict([(convert(key), convert(value)) for key, value in input.iteritems()])
+	elif isinstance(input, list):
+		return [convert(element) for element in input]
+	elif isinstance(input, unicode):
+		return input.encode('utf-8')
+	else:
+		return input
